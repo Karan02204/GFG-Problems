459681d21501e2d4a09e2104370df1eda1af8afa
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-22 09:53:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        // Your code here
+        map<int,int> m;
+        for(int i=0;i<a.size();i++){
+            m[a[i]]++;
+        }
+        
+        for(int i=0;i<b.size();i++){
+            if(m.find(b[i])==m.end() || m[b[i]]==0){
+                return false;
+            }
+            m[b[i]]--;
+        }
+        
+        return true;
+    }
+};
