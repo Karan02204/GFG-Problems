905aed775dfffdf0e52d73dce584d6e9fea61b55
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-02-15 19:41:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
@@ -1,33 +1,33 @@
 class Solution {
   public:
-    void DFS(int node , vector<vector<int>>& adj , vector<bool>& visited , vector<int>& temp){
+    void dfs(int node , vector<vector<int>>& adj , vector<bool>& visited , vector<int>& temp){
         visited[node] = true;
         temp.push_back(node);
         
-        for(int neigh : adj[node]){
-            if(!visited[neigh]){
-                DFS(neigh , adj , visited, temp);
+        for(auto neighbour : adj[node]){
+            if(!visited[neighbour]){
+                dfs(neighbour , adj , visited , temp);
             }
         }
     }
-    
     vector<vector<int>> getComponents(int V, vector<vector<int>>& edges) {
         // code here
         vector<vector<int>> adj(V);
-        for(auto &i : edges){
-            int u = i[0];
-            int v = i[1];
+        
+        for(auto& e : edges){
+            int u = e[0];
+            int v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         
-        vector<bool> visited(V);
+        vector<bool> visited(V,false);
         vector<vector<int>> result;
         
         for(int i=0;i<V;i++){
             vector<int> temp;
             if(!visited[i]){
-                DFS(i,adj,visited,temp);
+                dfs(i,adj,visited,temp);
                 result.push_back(temp);
             }
         }
