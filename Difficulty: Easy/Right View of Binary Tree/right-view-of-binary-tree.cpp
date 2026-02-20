f6fbcaa6d66a4af471b89f4f04edd1e9e715834c
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-02-20 10:49:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -0,0 +1,34 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void Helper(Node* root , vector<int>& result , int level){
+        if(!root) return;
+        if(result.size() == level) result.push_back(root->data);
+        
+        Helper(root->right , result , level+1);
+        Helper(root->left , result , level+1);
+    }
+    vector<int> rightView(Node *root) {
+        //  code here
+        vector<int> result;
+        if(!root) return result;
+        
+        Helper(root,result,0);
+        
+        return result;
+    }
+};
