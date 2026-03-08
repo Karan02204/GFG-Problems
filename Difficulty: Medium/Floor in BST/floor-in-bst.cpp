1be84abac5ffc55f5afe1f429421411d5fa9d526
Karan <134791021+Karan02204@users.noreply.github.com>
2026-03-08 17:15:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floor in BST/floor-in-bst.cpp
@@ -0,0 +1,37 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int floor(Node* root, int x) {
+        // code here
+        int floor = -1;
+        
+        while(root){
+            if(root->data == x){
+                floor = root->data;
+                return floor;
+            }
+            
+            if(x > root->data){
+                floor = root->data;
+                root = root->right;
+            } else{
+                root = root->left;
+            }
+        }
+        
+        return floor;
+    }
+};
