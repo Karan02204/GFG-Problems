1e55c70aeed5ba49753e6d1a189296e43d2e6569
Karan <134791021+Karan02204@users.noreply.github.com>
2026-03-15 12:50:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,23 +1,54 @@
 class Solution {
   public:
-    int countOccur(vector<int>& arr , int low , int high , int target){
-        if (low > high) return 0;
+    int helper_first(vector<int>& arr , int target){
+        int ans = -1;
         
-        int mid = (low+high)/2;
-        if(arr[mid] == target){
-            return 1+ countOccur(arr,low,mid-1,target) + countOccur(arr,mid+1,high,target);
-        } else if(arr[mid] > target){
-            countOccur(arr,low,mid-1,target);
-        } else{
-            countOccur(arr,mid+1,high,target);
+        int low = 0;
+        int high = arr.size() -1 ;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            
+            if(arr[mid] == target){
+                ans = mid;
+                high = mid - 1;
+            } else if(arr[mid] > target){
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
         }
         
+        return ans;
+    }
+    
+    int helper_last(vector<int>& arr , int target){
+        int ans = -1;
+        
+        int low = 0;
+        int high = arr.size() -1 ;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            
+            if(arr[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            } else if(arr[mid] > target){
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
     }
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int low = 0;
-        int high = arr.size()-1;
+        int first = helper_first(arr,target);
+        if(first == -1) return 0;
         
-        return countOccur(arr, low , high , target);
+        int last = helper_last(arr , target);
+        return last - first + 1;
     }
 };
